80ef8371e83e21251449cfbd74327a13eb111be6
alpha-hexor <itsalexwilliams69@gmail.com>
2022-02-04 00:11:22 -0800

    clean up

--- app.py
+++ app.py
@@ -1,40 +1,23 @@
-from tqdm import tqdm
-import base64
-import json
-import yarl
-from Cryptodome.Cipher import AES
-import requests
-from bs4 import BeautifulSoup
-import re
-from termcolor import colored
-import random
 import os
-from datetime import date,datetime
-import platform
+from termcolor import colored
+from tqdm import tqdm
+import random
 import sys
+from codebase.search import *
+from codebase.log import *
+from codebase.link_gen import *
+from codebase.parselog import *
 
-#global lists
-anime_links = []
-animes = []
+#create download directory
+if not os.path.exists('downloads'):
+    os.mkdir('downloads')
 
-headers = {"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"}
-main_url = "https://www1.gogoanime.pe/"
+#set mpv executable
+mpv_executable = "mpv.exe" if os.name == "nt" else "mpv"
 
-#create a download directory
-if not os.path.exists("downloads"):
-    os.mkdir("downloads")
-
-#function to set mpv executable
-if platform.system() == "Windows":
-    mpv_executable ="mpv.exe"
-else:
-    mpv_executable = "mpv"
-#clear function
+#clear screen function
 def clear():
-    if platform.system() == "Windows":
-        os.system("cls")
-    else:
-        os.system("clear")
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 #write a function to print colored text
 def colored_print(message):
@@ -42,348 +25,178 @@ def colored_print(message):
     color = random.choice(colors)
     print(colored(message,color,attrs=["bold"]))
 
+#function to get final link
+def get_final_link(embade_url):
+    qualities,links = generate_links(embade_url) 
 
-#padding
-def pad(data):
-    length = 16-(len(data)%16)
-    return data+chr(length)*length
-
-#function for get final shit link
-def parse_url(embade_url):
-    qualities = []
-    links=[]
-    s = b"257465385929383"b"96764662879833288"
-    p_url = yarl.URL(embade_url)
-    next_host = "https://{}/".format(p_url.host)
-    encrypted_ajax = base64.b64encode(AES.new(s, AES.MODE_CBC, iv=b'4206913378008135').encrypt(pad(p_url.query.get('id').replace('%3D', '=')).encode()))
-    c=(requests.get(
-        "{}encrypt-ajax.php".format(next_host),
-        params={
-            'id':encrypted_ajax.decode(),
-            'time':'69420691337800813569'
-        },
-        headers={'x-requested-with':'XMLHttpRequest'}
-    ))
-    j = json.loads(c.text)
-    for i in range(4):
-        try:
-            link = j['source'][i]['file']
-            links.append(link)
-            q = j['source'][i]['label']
-            qualities.append(q)
-        except:
-            pass
-    colored_print("[*]Availabel qualities")
+    colored_print("[*]Available Qualities: ")
     for i in range(len(qualities)):
-        colored_print("["+str(i+1)+"]"+qualities[i])
-    x = int(input("[*]Enter Index: "))
-    x -= 1
-    return links[x]
+        colored_print("["+str(i+1)+"] "+qualities[i])
+    opt = int(input("[*]Enter index of the quality: "))
+    return links[opt-1]
 
-
-
-#function to search anime
-def search_anime(name):
-    if len(name) > 1:
-        name = name.replace(" ","-")
-    search_url = main_url + "//search.html?keyword="+name
-    r = requests.get(search_url,headers=headers)
-    src = r.content
-    soup = BeautifulSoup(src,'lxml')
-    hrefs = soup.find_all("p",attrs={'class':'name'})
-
-
-    #get all the links
-    for h in hrefs:
-        tags = str(h)
-        link = tags.split('/')[2].split('"')[0]
-        anime_links.append(link)
-
-    #for the names
-    for href in hrefs:
-        href = str(href)
-        anime_name = re.sub('<[^>]*>', '', href)
-        animes.append(str(anime_name))
-
-#function to search last and first episode
-def search_episode(name):
-    ep_url = main_url + "/category/"+name
-    #print(ep_url)
-    k = requests.get(ep_url,headers=headers)
-    src2 = k.content
-    soup2 = BeautifulSoup(src2,'lxml')
-    eps = soup2.find("a",attrs={'href':'#',"class":"active"}).text
-    first_episode = eps.split("-")[0]
-    first_episode = int(first_episode)+1
-    try:
-        last_episode = eps.split("-")[1]
-        last_episode = int(last_episode)
-    except:
-        last_episode = 1
-    return(first_episode,last_episode)
-
-#create a watch log
-def create_log(name,ep_number,last_ep):
-    current_date = date.today()
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    with open("watch_log.txt","a") as f:
-        f.write("["+str(current_date) + ":" + current_time + "] Starting " + name+": episode-"+ep_number+":"+str(last_ep)+"\n")
-    f.close()
-
-#create a download log
-def download_log(name,ep_number,last_ep):
-    current_date = date.today()
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    with open("download_log.txt","a") as f:
-        f.write("["+str(current_date) + ":" + current_time + "] Downloaded " + name+": episode-"+ep_number+":"+str(last_ep)+"\n")
-    f.close()
-
-
-#get embade url
-def get_embade_link(name,ep_num):
-    link = main_url + "/"+name+"-episode-"+ep_num
-    try:
-        m = requests.get(link,headers=headers)
-    except:
-        main_url + "/"+name+"-episode-0"
-        m=requests.get(link,headers=headers)
-    src3 = m.content
-    soup3 = BeautifulSoup(src3,'lxml')
-    
-    for item in soup3.find_all('a', attrs={'href':'#',"rel":"100",'data-video' : True}):
-        url = str(item['data-video'])
-    url = "https:" + url
-    return url
-
-#function to stream episode
+#stream function
 def stream_episode(name,ep_num,last_ep):
     clear()
-    colored_print("[*]Streaming episode: " +name+":"+ep_num)
-    
+    colored_print("[*]Streaming Episode: "+name+": episode-"+ep_num)
+
     embade_url = get_embade_link(name,ep_num)
-    link = parse_url(embade_url)
-    #print(link)
+    link = get_final_link(embade_url)
+    
+    #stream episode
     command = ' "'+link+'"'
     os.system(mpv_executable+command)
+
+    #for next episode
     if (int(ep_num) + 1 <= int(last_ep)):
         opt = input(("[*]Want to start next episode[y/n]: "))
-    
+        
 
-        if (opt == "y"):
-            
-            ep_num = int(ep_num)+1
-            create_log(name,str(ep_num),str(last_ep))
-            stream_episode(name,str(ep_num),last_ep)
-        else:
+        if (opt == "n"):
             exit()
+                
+        ep_num = int(ep_num)+1
+        watch_log(name,str(ep_num),str(last_ep))
+        stream_episode(name,str(ep_num),last_ep)
+    
     else:
         exit()
 
-#fucntion to download episode
+#download function
 def download_episode(path,name,ep_num,last_ep):
     clear()
-    colored_print("[*]Downloading episode: " +name+":"+ep_num)
+    colored_print("[*]Downloading Episode: "+name+": episode-"+ep_num)
     embade_url = get_embade_link(name,ep_num)
-    link = parse_url(embade_url)
+    link = get_final_link(embade_url)
+
+    #download process
     r=requests.get(link,headers={'referer':"https://gogoplay.io"},stream=True)
     total_size_in_bytes= int(r.headers.get('content-length', 0))
-    block_size = 1024 #1 kilobyte data
+    block_size = 1024
     progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
     with open(path+"\\"+name+"_"+ep_num+".mp4", 'wb') as f:
         for data in r.iter_content(block_size):
             progress_bar.update(len(data))
-    
             f.write(data)
     f.close()
     progress_bar.close()
 
-    
-
+    #for next episode
     if (int(ep_num) + 1 <= int(last_ep)):
-        opt = input(("[*]Want to start next episode[y/n]: "))
-    
+        opt = input(("[*]Want to download next episode[y/n]: "))
+        
 
-        if (opt == "y"):
-            
-            ep_num = int(ep_num)+1
-            download_log(name,str(ep_num),str(last_ep))
-            download_episode(path,name,str(ep_num),last_ep)
-        else:
+        if (opt == "n"):
             exit()
+                
+        ep_num = int(ep_num)+1
+        download_log(name,str(ep_num),str(last_ep))
+        download_episode(path,name,str(ep_num),last_ep)
     else:
         exit()
 
-#function for continue streaming
-def continue_stream():
-    anime = {}
-    anime_last = {}
-    names = []
-    if not os.path.exists("watch_log.txt"):
-        print("[*]No watch log found")
-        exit()
-    else:
-        #parse the watch log
-        with open("watch_log.txt","r") as f:
-           
-
-            for line in f.readlines():
-                #print(line)
-                p = line.strip("\n")
-               
-                if len(p) > 1:
-                    s= p.split(" ")
-                    name = s[2]
-                    name = name.replace(":","")
-                    names.append(name)
-                    episode_watched = s[-1].split("-")[-1].split(":")[0]
-                    
-                    last_episode = s[-1].split("-")[-1].split(":")[1]
-                    
-                    if str(episode_watched) != str(last_episode):
-                        anime[name] = str(episode_watched)
-                        anime_last[name] = str(last_episode)
-
-                       
-                    else:
-                        anime[name] = "watched"
-        f.close()
-        #delete duplicate names
-        n=set(names)
-        names = list(n)
-
-        #delete already finished anime names
-        for i in names:
-            if anime[i] == "watched":
-                names.remove(i)
-        
-        #take user input
-        colored_print("[*]Continue Watching...........")
-        for n in range(len(names)):
-            colored_print("["+str(n+1)+"] "+names[n])
-        
-        x=int(input("[*]Enter index of anime to watch: "))
-        x-=1
-        anime_to_watch = names[x]
-        episode_to_watch = int(anime[anime_to_watch]) + 1
-        
-        create_log(anime_to_watch,str(episode_to_watch),str(anime_last[anime_to_watch]))
-        stream_episode(anime_to_watch,str(episode_to_watch),str(anime_last[anime_to_watch]))
-
-#function for continue downloading
-def continue_download():
-    anime = {}
-    anime_last = {}
-    names = []
-    if not os.path.exists("download_log.txt"):
-        colored_print("[*]No watch log found")
-        exit()
-    else:
-        #parse the watch log
-        with open("download_log.txt","r") as f:
-           
-
-            for line in f.readlines():
-                #print(line)
-                p = line.strip("\n")
-               
-                if len(p) > 1:
-                    s= p.split(" ")
-                    name = s[2]
-                    name = name.replace(":","")
-                    names.append(name)
-                    episode_watched = s[-1].split("-")[-1].split(":")[0]
-                    
-                    last_episode = s[-1].split("-")[-1].split(":")[1]
-                    
-                    if str(episode_watched) != str(last_episode):
-                        anime[name] = str(episode_watched)
-                        anime_last[name] = str(last_episode)
-
-                       
-                    else:
-                        anime[name] = "downloaded"
-        f.close()
-        #delete duplicate names
-        n=set(names)
-        names = list(n)
-
-        #delete already finished anime names
-        for i in names:
-            if anime[i] == "downloaded":
-                names.remove(i)
-        
-        #take user input
-        colored_print("[*]Continue Downloading...........")
-        for n in range(len(names)):
-            colored_print("["+str(n+1)+"] "+names[n])
-        
-        x=int(input("[*]Enter index of anime to download: "))
-        x-=1
-        anime_to_watch = names[x]
-        episode_to_watch = int(anime[anime_to_watch]) + 1
-        path = "downloads\\" + anime_to_watch
-        download_log(anime_to_watch,str(episode_to_watch),str(anime_last[anime_to_watch]))
-        download_episode(path,anime_to_watch,str(episode_to_watch),str(anime_last[anime_to_watch]))
-
-
-#main function
 def main():
     if len(sys.argv) == 1:
-        try:
-            #get anime name
-            name = input("[*]Enter Anime Name: ")
-            search_anime(name)
+        name = input("[*]Enter anime name: ")
+        animes,anime_links =search_anime(name)
 
-            #print all the names
-            for i in range(len(animes)):
-                colored_print("["+str(i+1)+"] "+animes[i])
-            
-            #take input
-            opt = input("[*]Enter the index of the anime you want to watch: ")
-            animes_to_watch = anime_links[int(opt)-1]
+        for i in range(len(animes)):
+            colored_print("["+str(i+1)+"] "+animes[i])
+        
+        #take input from user
+        opt = int(input("[*]Enter index of the anime: "))
+        anime_to_watch = anime_links[opt-1]
+        
+        #get episode list
+        first_episode,last_episode = search_episode(anime_to_watch)
 
-            #get first and last episode
-            first_ep,last_ep = search_episode(animes_to_watch)
-            
+        colored_print("[*]Available Episode: [" + str(first_episode) + "-"+str(last_episode)+"]")
 
-            colored_print("[*]Available Episode: [" + str(first_ep) + "-"+str(last_ep)+"]")
+        #take episode num input
+        ep_num = input("[*]Enter episode number: ")
 
-            #take episode num as input
-            ep_num = input("[*]Enter the episode number you want to watch: ")
-
-            if((int(ep_num) >= 0) and (int(ep_num) <= last_ep)):
-                clear()
-                colored_print('[S]tream Episode')
-                colored_print('[D]ownload Episode')
-                x = input("[*]Enter your choice: ")
-                if(x == "D" or x == "d"):
-                    path = "downloads\\" + animes_to_watch
-                    if not os.path.exists(path):
-                        os.makedirs(path)
-                    download_log(animes_to_watch,ep_num,last_ep)
-                    download_episode(path,animes_to_watch,ep_num,last_ep)
-                else:
-                    create_log(animes_to_watch,ep_num,last_ep)
-                    stream_episode(animes_to_watch,ep_num,last_ep)
+        if int(ep_num) >= 0 and int(ep_num) <= last_episode:
+            clear()
+            colored_print('[S]tream Episode')
+            colored_print('[D]ownload Episode')
+            x = input("[*]Enter your choice: ")
+            if x == 'd' or x == 'D':
+                path = "downloads\\" + anime_to_watch
+                if not os.path.exists(path):
+                    os.makedirs(path)
+                download_log(anime_to_watch,ep_num,last_episode)
+                download_episode(path,anime_to_watch,ep_num,last_episode)
             else:
-                colored_print("[*]Invalid episode number")
-                exit()
-        except Exception as e:
-            colored_print(e)
+                watch_log(anime_to_watch,ep_num,last_episode)
+                stream_episode(anime_to_watch,ep_num,last_episode)
+        else:
+            colored_print("[*]Invalid episode number")
             exit()
+
     else:
         if sys.argv[1] == "--help" or sys.argv[1] == "-h":
+            '''
+            --help implementation
+            '''
             colored_print("[*]Usage: python app.py --help [for help]")
             colored_print("[*]Usage: python app.py [for normal usage]")
             colored_print("[*]Usage: python app.py --continue_stream [continue watch animes]")
             colored_print("[*]Usage: python app.py --continue_download [continue download animes]")
             
             exit()
-        if sys.argv[1] == "--continue_stream" :
-            continue_stream()
-        if sys.argv[1] == "--continue_download":
-            continue_download()
         
-main()
+        
+        if sys.argv[1] == "--continue_stream" :
+
+            '''
+            --continue_stream implementation
+            '''
+
+            #check for log file
+            if not os.path.exists("watch_log.txt"):
+                colored_print("[*]No watch log found")
+                exit()
+
+            anime,anime_last,names=parse_log("watch_log.txt")
+            
+            #take user input
+            colored_print("[*]Continue Watching.........")
+            for n in range(len(names)):
+                colored_print("["+str(n+1)+"] "+names[n])
+            
+            x=int(input("[*]Enter index of anime to watch: "))
+            anime_to_watch = names[x-1]
+            episode_to_watch = int(anime[anime_to_watch]) + 1
+
+            watch_log(anime_to_watch,str(episode_to_watch),anime_last[anime_to_watch])
+            stream_episode(anime_to_watch,str(episode_to_watch),anime_last[anime_to_watch])
+        
+        
+        if sys.argv[1] == "--continue_download":
+            '''
+            --continue_download implementation
+            '''
+            
+            #check for log file
+            if not os.path.exists("download_log.txt"):
+                colored_print("[*]No download log found")
+                exit()
+                
+            anime,anime_last,names=parse_log("download_log.txt")
+
+            #take user input
+            colored_print("[*]Continue Downloading.........")
+            for n in range(len(names)):
+                colored_print("["+str(n+1)+"] "+names[n])
+            
+            x=int(input("[*]Enter index of anime to download: "))
+            anime_to_download = names[x-1]
+            episode_to_download = int(anime[anime_to_download]) + 1
+            path = "downloads\\" + anime_to_download
+
+            download_log(anime_to_download,str(episode_to_download),anime_last[anime_to_download])
+            download_episode(path,anime_to_download,str(episode_to_download),anime_last[anime_to_download])
+
+
+if __name__ == "__main__":
+    main()
---
+++ codebase/parselog.py
@@ -0,0 +1,40 @@
+def parse_log(f):
+
+    '''
+    function to parse log file
+    '''
+    
+    anime = {}
+    anime_last = {}
+    names = []
+
+
+    with open(f,"r") as f:
+        for line in f.readlines():
+            p= line.strip("\n")
+
+            if len(p)>1:
+                s = p.split(" ")
+                name = s[2]
+                name = name.replace(":","")
+                names .append(name)
+                episode_watched = s[-1].split("-")[-1].split(":")[0]
+                last_episode = s[-1].split("-")[-1].split(":")[1]
+
+                if episode_watched != last_episode:
+                    anime[name] = episode_watched
+                    anime_last[name] = last_episode
+                else:
+                    anime[name] = "finished"
+    f.close()
+
+    #delete duplicates if any
+    n= set(names)
+    names = list(n)
+
+    #delete already finished anime
+    for i in names:
+        if anime[i] == "finished":
+            names.remove(i)
+
+    return anime,anime_last,names
